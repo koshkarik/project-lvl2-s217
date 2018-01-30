dd859473014b64236c7a10dc97d3058f80152e74
Koshkarik <koshkarevj@mail.ru>
2018-01-30 12:16:10 +0300

    buildFile small fix

--- src/classes/buildFile.js
+++ src/classes/buildFile.js
@@ -2,8 +2,13 @@ import path from 'path';
 import JsonObj from './JsonObj';
 import YmlObj from './YmlObj';
 
+const decideObj = {
+  json: file => new JsonObj(file),
+  yml: file => new YmlObj(file),
+};
+
 export default (file) => {
   const base = path.basename(file);
-  const ext = path.extname(base);
-  return ext === '.json' ? new JsonObj(file) : new YmlObj(file);
+  const ext = path.extname(base).substring(1);
+  return decideObj[ext](file);
 };
